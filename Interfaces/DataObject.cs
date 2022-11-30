f10fc244ad95f6857fa54bf66567ea39f30effd4
Armand Marechal <sypenfu@gmail.com>
2022-11-30 10:35:24 +0100

    update .gitignore tracking

---
+++ Interfaces/DataObject.cs
@@ -0,0 +1,24 @@
+namespace MyMicroservice;
+
+public class DataObject : IDataObject
+{
+    public bool DoesExist(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Create(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Download(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Publish(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Interfaces/IDataObject.cs
@@ -0,0 +1,12 @@
+namespace MyMicroservice;
+
+public interface IDataObject
+{
+    public bool DoesExist(string path);
+
+    public void Create(object data);
+
+    public object Download(string path);
+
+    public void Publish(object data);
+}
