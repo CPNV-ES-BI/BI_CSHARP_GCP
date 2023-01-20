6e0ef1730f963d97f60a77ef4813a7d2cde0633e
Armand Marechal <sypenfu@gmail.com>
2023-01-20 20:14:48 +0100

    Add Create route

---
+++ GCPMicroservice/Controllers/DataObjectController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCPMicroservice.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DataObjectController : ControllerBase
+{
+    private GCPDataObject _dataObject;
+
+    public DataObjectController()
+    {
+        _dataObject = new();
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateDataObject(string key, string base64Content)
+    {
+        try {
+            byte[] content = Convert.FromBase64String(base64Content);
+
+            await _dataObject.Create(key, content);
+            return Ok();
+        }
+        catch (FormatException e)
+        {
+            return BadRequest("Invalid base64 string");
+        }
+    }
+}
